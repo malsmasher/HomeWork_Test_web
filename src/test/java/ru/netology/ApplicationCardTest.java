7f81d6686a4c5a3167da7c6d6914a63d87644021
m.malyshonok <masha.malyshonok@gmail.com>
2024-09-02 11:49:05 +0500

    заново установила webdriver, проверила и исправила селекторы

--- src/test/java/ru/netology/ApplicationCardTest.java
+++ src/test/java/ru/netology/ApplicationCardTest.java
@@ -1,5 +1,4 @@
 package ru.netology;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.By.cssSelector;
 
+
 public class ApplicationCardTest {
     private WebDriver driver;
 
@@ -23,14 +23,17 @@ public class ApplicationCardTest {
         WebDriverManager.chromedriver().setup();
     }
 
+
     @BeforeEach
-    public void setup() {
+    public void setUp() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
+
     }
 
     @AfterEach
@@ -40,15 +43,17 @@ public class ApplicationCardTest {
     }
 
     @Test
-    public void shouldBeSuccessfulForm() {
-        driver.findElement(By.cssSelector("[data-test-id =`name`] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id =`phone`] input")).sendKeys("+79000000000");
-        driver.findElement(By.cssSelector("[data-tes-id =`agreement`]")).click();
-        driver.findElement(By.cssSelector("button.button")).click();
+    public void shoulBeSuccsses() throws InterruptedException {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79105004050");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+
         WebElement actualElement = driver.findElement(cssSelector("[data-test-id=order-success]"));
         String actualText = actualElement.getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualElement.isDisplayed());
-    }
-}
 
+    }
+
+}
